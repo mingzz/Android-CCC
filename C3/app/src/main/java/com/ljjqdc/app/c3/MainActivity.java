7f1fd896dca906ff355fab9351dba2c63c965a54
wcgboy <sjtudl@sjtu.edu.cn>
2014-10-21 19:56:53 +0800

    Hello AnyChat demo

--- C3/app/src/main/java/com/ljjqdc/app/c3/MainActivity.java
+++ C3/app/src/main/java/com/ljjqdc/app/c3/MainActivity.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.helloanychat.ChatMainActivity;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -30,7 +35,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initBlueTooth();
+        //initBlueTooth();
+
+        Button button = (Button)findViewById(R.id.btn_test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChatMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initBlueTooth(){
